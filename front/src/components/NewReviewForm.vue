ac821f056e2b140e9086833882842b579b42cb8a
inerttila <inert.etila@gmail.com>
2024-11-22 11:19:49 +0100

    add

--- front/src/components/NewReviewForm.vue
+++ front/src/components/NewReviewForm.vue
@@ -116,8 +116,9 @@ const handleOptionsChange = (option) => {
               required
               class="block text-black w-full ps-2 rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               @change="handleOptionsChange($event.target.value)"
+              v-model="optionsInput"
             >
-              <option value="default" disabled selected>Select the Shop</option>
+              <option value="default" disabled>Select the Shop</option>
               <option value="skaitech">SKAITECH</option>
               <option value="3dskai">3DSKAI</option>
               <option value="other">Other</option>
