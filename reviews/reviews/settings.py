898cbbed5b7eff2c663cce0e55ad6b89370bfa4a
inerttila <inert.etila@gmail.com>
2025-01-09 17:12:25 +0100

    add a template for the login and api

--- reviews/reviews/settings.py
+++ reviews/reviews/settings.py
@@ -60,7 +60,8 @@ ROOT_URLCONF = 'reviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'reviews' / 'templates',         ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ reviews/reviews/views.py
@@ -0,0 +1,6 @@
+# reviews/views.py
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html') 
